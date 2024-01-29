67be39fd030c5a69d5a969204a96e8e4569da40f
Mayara Lucy Albano de Deus <mayaralucy@mayaras-mbp.br.ibm.com>
2024-01-29 09:42:22 -0300

    Versão 1

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -4,6 +4,24 @@ import QRcode from "../../images/Clip path group.png";
 import Carrinho from "../../images/carrinho frente.png";
 import CarTras from "../../images/carrinho trás.png";
 import rack from "../../images/estante.png";
+import ImageList from "../../components/snacks.js";
+
+import SnackTur1 from "../../images/Turbonomic/turbonomic 1.png"
+import SnackTur2 from "../../images/Turbonomic/turbonomic 2.png";
+import SnackTur3 from "../../images/Turbonomic/turbonomic 3.png";
+
+import SnackIns1 from "../../images/Instana/instana 1.png";
+import SnackIns2 from "../../images/Instana/instana 2.png";
+import SnackIns3 from "../../images/Instana/instana 3.png";
+
+import SnackMax1 from "../../images/Maximo/maximo 1.png";
+import SnackMax2 from "../../images/Maximo/maximo 2.png";
+import SnackMax3 from "../../images/Maximo/maximo 3.png";
+
+import SnackTurFinal1 from "../../images/Turbonomic/turbonomic 1.png";
+import SnackTurFinal2 from "../../images/Turbonomic/turbonomic 2.png";
+import SnackTurFinal3 from "../../images/Turbonomic/turbonomic 3.png";
+
 
 import {
   Filter,
@@ -18,7 +36,7 @@ export default function Cover() {
   return (
     <div id="background">
       <div id="headerInfo">
-        <img src={Innovation} style={{width: '10%'}} />
+        <img src={Innovation} style={{ width: '10%' }} />
         <div className="headerPai">
           <div className="headerIrmã">
             <div className="headerTexts">
@@ -39,14 +57,14 @@ export default function Cover() {
               </div>
             </div>
             <div className="headerQrCode">
-              <img src={QRcode} style={{width: '100%'}}/>
+              <img src={QRcode} style={{ width: '100%' }} />
             </div>
           </div>
           <div className="carrinho">
             <div id="carrinhoFrente">
               <img id="imgCarrinhoFrente" src={Carrinho} /> </div>
             <div id="carrinhoTras">
-                <img id="imgCarrinhoTras" src={CarTras} />
+              <img id="imgCarrinhoTras" src={CarTras} />
             </div>
           </div>
         </div>
@@ -54,8 +72,33 @@ export default function Cover() {
       <div className="BackImg">
         <img id="estante" src={rack} alt="Background"></img>
       </div>
-    </div>
+      <div className="fileira" style={{ width: "50%", position: "absolute" }}>
+        <div id="Snacks1" style={{ width: "50%", position: "absolute" }}>
+          <img src={SnackTur1} style={{ width: "10%" }}></img>
+          <img src={SnackTur2} style={{ width: "10%" }}></img>
+          <img src={SnackTur3} style={{ width: "10%" }}></img>
+        </div>
+        <div id="Snacks2" style={{ width: "50%", position: "absolute" }}>
+          <img src={SnackIns1} style={{ width: "10%" }} ></img>
+          <img src={SnackIns2} style={{ width: "10%" }} ></img>
+          <img src={SnackIns3} style={{ width: "10%" }} ></img>
+        </div>
+        <div id="Snacks3" style={{ width: "50%", position: "absolute" }}>
+          <img src={SnackMax1} style={{ width: "10%" }} ></img>
+          <img src={SnackMax2} style={{ width: "10%" }} ></img>
+          <img src={SnackMax3} style={{ width: "10%" }} ></img>
+        </div>
+        <div id="Snacks4" style={{ width: "50%", position: "absolute" }}>
+          <img src={SnackTurFinal1} style={{ width: "10%" }} ></img>
+          <img src={SnackTurFinal2} style={{ width: "10%" }} ></img>
+          <img src={SnackTurFinal3} style={{ width: "10%" }} ></img>
 
+        </div>
+
+
+      </div>
+      {/* <div id="Snacks"> */}
+      {/* <ImageList /> */}
+    </div>
   )
 };
-
